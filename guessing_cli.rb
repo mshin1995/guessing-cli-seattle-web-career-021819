f7548fe6fbe8883168d25a78b5168b6abc257a59
Matthew Shin <mshin1995@gmail.com>
2019-02-20 05:36:18 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -4,6 +4,7 @@ def run_guessing_game
   random_num = rand(1..6)
   if user_input == random_num.to_s
     puts "You guessed the correct number!"
+    return exit
   elsif user_input != random_num.to_s
     puts "The computer guessed #{random_num}."
   end
